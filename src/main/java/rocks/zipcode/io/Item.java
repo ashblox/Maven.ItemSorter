03d8d869fb4656c7ea5b2df19ce8ab21670e8e40
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-02-25 13:15:28 -0500

    finished method stubs

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,44 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+
+    private long id;
+    private String name;
+    private double price;
+
     public Item() {
+        id = 1l;
+        name = "myname";
+        price = 1.0d;
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
